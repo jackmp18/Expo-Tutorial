dcf290a6cb387ff6a05d22fba0ca48cb90051787
jackmp18 <55199733+jackmp18@users.noreply.github.com>
2025-05-22 12:04:56 -0700

    Final Added buttona and made them pressable and stylized

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,15 +1,19 @@
 import { View, StyleSheet } from 'react-native';
- import { Image } from 'expo-image'; 
 
+import Button from '@/components/button';
+import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
-
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -24,9 +28,8 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
